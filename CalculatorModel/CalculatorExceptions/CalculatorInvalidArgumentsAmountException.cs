24dfcef94d0609bdcbcfa3b3f13a9b2c9dcc3d65
mkolesnyova <marina.kolesnyova@gmail.com>
2025-03-14 14:50:14 +0300

    Added CalculatorInvalidArgumentsAmountException. Modified references to CalculatorModel.

---
+++ CalculatorModel/CalculatorExceptions/CalculatorInvalidArgumentsAmountException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorModel.CalculatorExceptions
+{
+    public class CalculatorInvalidArgumentsAmountException : CalculatorException 
+    {
+        public CalculatorInvalidArgumentsAmountException() 
+        {
+        
+        }
+
+        public CalculatorInvalidArgumentsAmountException(string message)
+            : base(message) 
+        {
+        
+        }
+
+        public CalculatorInvalidArgumentsAmountException(string message, Exception inner) 
+            : base(message, inner)
+        {
+
+        }
+    }
+}
